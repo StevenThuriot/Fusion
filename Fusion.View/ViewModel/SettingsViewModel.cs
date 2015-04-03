213f6a86f5872af23d229de1e0dd01e41654e46d
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-03 18:36:12 +0200

    Update LICENSE

--- Fusion.View/ViewModel/SettingsViewModel.cs
+++ Fusion.View/ViewModel/SettingsViewModel.cs
@@ -1,29 +1,11 @@
-﻿#region License
-// 
-//  Copyright 2012 Steven Thuriot
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// 
-#endregion
-using System;
+﻿using System;
 using System.ComponentModel;
-using System.Windows;
-using Fusion.View.Actions.Settings;
-using Nova.Base;
-using Fusion.View.Views;
-using Fusion.Domain;
 using System.Security;
+using System.Windows;
+using Fusion.Domain;
+using Fusion.View.Actions.Settings;
 using Fusion.View.Properties;
+using Fusion.View.Views;
 
 namespace Fusion.View.ViewModel
 {
